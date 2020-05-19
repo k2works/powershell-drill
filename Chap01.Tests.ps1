e6b4dd37e0fa0151aead126afec40dfad7d667a2
k2works <kakimomokuri@gmail.com>
2020-05-19 10:17:30 +0900

    feat: ３値の最大値

--- Chap01.Tests.ps1
+++ Chap01.Tests.ps1
@@ -24,15 +24,3 @@ Describe "３値の最大値" {
         Max3 $a $b $c | Should -Be $expected
     }
 }
-
-function Max3 {
-    param (
-        [int] $a,
-        [int] $b,
-        [int] $c
-    )
-    $result = $a
-    if ($b -gt $result) { $result = $b }
-    if ($c -gt $result) { $result = $c }
-    return $result
-}
--- Chap01.ps1
+++ Chap01.ps1
@@ -1,3 +1,11 @@
-function Chap01 {
-
+function Max3 {
+    param (
+        [int] $a,
+        [int] $b,
+        [int] $c
+    )
+    $result = $a
+    if ($b -gt $result) { $result = $b }
+    if ($c -gt $result) { $result = $c }
+    return $result
 }
